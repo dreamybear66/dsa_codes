4591d891a0e6cad2f492582e4c57acf5c42f9d40
Krrish Agarwal <dreamybear66@gmail.com>
2025-12-28 01:55:44 +0530

    implemented push using linked list

---
+++ stacks/pushll.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+}*top = NULL;
+
+void push(int value){
+    Node *t = new Node;
+    if(t == NULL){
+        cout << "stack overflow" << endl;
+    }
+    else{
+        t->data = value;
+        t->next = top;
+        top = t;
+    }
+}
